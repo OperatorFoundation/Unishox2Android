a5f39267eef9fd519d3ed21e5c3de8462ee8376c
consuelita <adelita@operatorfoundation.org>
2025-10-20 16:57:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Unishox2Android
+Unishox2Android is a Kotlin wrapper for the Unishox2 text compression algorithm.
---
+++ Unishox2/src/main/java/org/operatorfoundation/unishox2/Unishox2.kt
@@ -0,0 +1,14 @@
+package org.operatorfoundation.unishox2
+
+class Unishox2
+{
+    fun compress(text: String): ByteArray
+    {
+        return byteArrayOf()
+    }
+
+    fun decompress(compressed: ByteArray): String
+    {
+        return ""
+    }
+}
